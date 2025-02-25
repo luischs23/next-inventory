98c1014c39af76c3866e93f9ead63047d6d57f09
luischs23 <luisc.herreras@gmail.com>
2025-02-25 11:00:05 -0500

    fix 2

--- src/app/api/check-permissions/route.ts
+++ src/app/api/check-permissions/route.ts
@@ -1,19 +1,11 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
-import { adminAuth } from "app/services/firebase/firebaseAdmin"
+import { auth } from "app/services/firebase/firebase.config";
 
 export async function GET(req: NextRequest) {
-  const authHeader = req.headers.get("Authorization");
-  if (!authHeader?.startsWith("Bearer ")) {
+  if (!auth.currentUser) {
     return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
   }
 
-  const token = authHeader.split("Bearer ")[1];
-
-  try {
-    const decodedToken = await adminAuth.verifyIdToken(token);
-    return NextResponse.json({ message: "Acceso permitido", uid: decodedToken.uid });
-  } catch (error) {
-    return NextResponse.json({ error: "Invalid token" }, { status: 401 });
-  }
+  return NextResponse.json({ message: "Acceso permitido" });
 }
